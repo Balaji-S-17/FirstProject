117103a04131f2d49a786e1ea3672ebc98e0ede2
Balaji-S-17 <balaji.s.250104.com>
2025-08-04 22:30:31 +0530

    changed to i am praveen

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,6 @@
 public class HelloWorld{
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("I Am Balaji");
-=======
+
         System.out.println("I Am Praveen!");
->>>>>>> dd4c51f6da52b2e3f44114405ce062903c968740
     }
 }
